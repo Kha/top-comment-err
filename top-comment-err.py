09e74bb093bfc431bc53a4776dae569e24c1dd2a
Sebastian Ullrich <sebasti@nullri.ch>
2013-12-08 23:32:18 +0100

    I am bored.

--- top-comment-err.py
+++ top-comment-err.py
@@ -1,8 +1,9 @@
 # vim:noet:sw=4:ts=4
 
-from errbot import BotPlugin
+from errbot import BotPlugin, botcmd
 import praw
 import bs4
+import random
 
 r = praw.Reddit(user_agent='top-comment-err 1.0 by /u/derKha')
 
@@ -16,20 +17,33 @@ def unescape_html(encoded):
 
 MAX_LEN = 500
 
+def format_comment_body(comment):
+	body = unescape_html(comment.body_html)
+	if len(body) > MAX_LEN:
+		soup = bs4.BeautifulSoup(body[:MAX_LEN])
+		soup.div.append(bs4.BeautifulSoup('<p><a href="{0}">...</a></p>'.format(comment.permalink)))
+		body = str(soup)
+	return body
+
 def get_top_comment(url):
 	submissions = r.get_info(url)
 	for s in sorted(submissions, key=lambda s: s.score, reverse=True):
 		if s.comments:
 			max_comment = max(s.comments, key=lambda c: getattr(c, 'score', -1))
 			if max_comment.score > 10:
-				body = unescape_html(max_comment.body_html)
-				if len(body) > MAX_LEN:
-					soup = bs4.BeautifulSoup(body[:MAX_LEN])
-					soup.div.append(bs4.BeautifulSoup('<p><a href="{0}">...</a></p>'.format(max_comment.permalink)))
-					body = str(soup)
-				return body
+				return format_comment_body(max_comment)
 
 	return None
+
+def get_top(subreddit):
+	# some praw bug, I don't care
+	res = []
+	try:
+		for submission in r.get_top(subreddit):
+			res.append(submission)
+	except TypeError:
+		pass
+	return res
 
 class TopCommentErr(BotPlugin):
 	def __init__(self):
@@ -41,3 +55,13 @@ class TopCommentErr(BotPlugin):
 			if top_comment:
 				self.send(mess.getFrom(), top_comment, message_type=mess.getType())
 				return
+
+	@botcmd
+	def bored(self, mess, args):
+		submissions = [s for s in r.get_subreddit('AskReddit').get_top(params={'t': 'week'}) if s.score > 1000 and 'NSFW' not in s.title]
+		if submissions:
+			s = random.choice(submissions)
+			comments = [c for c in s.comments if getattr(c, 'score', -1) > 300]
+			if comments:
+				c = random.choice(comments)
+				return '<div><b>{}</b><br/>{}</div>'.format(s.title, format_comment_body(c))
